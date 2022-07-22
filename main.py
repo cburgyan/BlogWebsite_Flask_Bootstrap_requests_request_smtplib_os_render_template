43e7adc83711e140b33a657b105e1e9a0608033c
cburgyan <cburgyan@gmail.com>
2022-07-22 16:05:33 -0500

    use jinja of flask to input blog posts in index.html

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 
+URL = 'https://api.npoint.io/ec84679d2403beee8160'
+response_json = requests.get(url=URL).json()
+# print(response_json)
+
+
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', blog_posts=response_json)
+
 
 @app.route('/about.html')
 def about_page():
     return render_template('about.html')
 
+
 @app.route('/contact.html')
 def contact_page():
     return render_template('contact.html')
+
 
 @app.route('/post.html')
 def post_page():
