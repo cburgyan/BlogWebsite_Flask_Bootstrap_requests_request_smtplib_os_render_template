92e9016d0f7c86fb22a114cab30d46e1b296bb28
cburgyan <cburgyan@gmail.com>
2022-07-22 14:55:55 -0500

    use templates for header and footer of all pages and connect navigation bar hyperlinks

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
+@app.route('/about.html')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact_page():
+    return render_template('contact.html')
+
+@app.route('/post.html')
+def post_page():
+    return render_template('post.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
