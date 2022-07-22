c44c1f564cde7a03872f78a189ac0ea86ab24bb6
cburgyan <cburgyan@gmail.com>
2022-07-22 18:35:07 -0500

    add dynamic url building in post.html and main.py and add photos for post.html

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 
 
-URL = 'https://api.npoint.io/ec84679d2403beee8160'
+URL = 'https://api.npoint.io/98b105151915c6944658'
 response_json = requests.get(url=URL).json()
 # print(response_json)
 
@@ -25,9 +25,16 @@ def contact_page():
     return render_template('contact.html')
 
 
-@app.route('/post.html')
-def post_page():
-    return render_template('post.html')
+@app.route('/post/<blog_id>')
+def post_page(blog_id):
+    page_post = {}
+    for post in response_json:
+        print(post)
+        if post['id'] == int(blog_id):
+            page_post = post
+            break
+
+    return render_template('post.html', page_post=page_post)
 
 
 if __name__ == "__main__":
